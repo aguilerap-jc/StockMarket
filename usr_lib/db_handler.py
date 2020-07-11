4d2cfdf4c6ef0edc5278b57b122a7e31b0f77fe1
aguilerap-jc <aguilerapjc@gmail.com>
2020-07-11 14:56:11 -0500

    Updating tab and spacing

--- usr_lib/db_handler.py
+++ usr_lib/db_handler.py
@@ -44,11 +44,16 @@ def get_stock_yf(stock='TSLA',start_date=DEFAULT_START_DATE,end_date=DEFAULT_END
 
 def get_stock_quandl(stock='AAPL',db = "WIKI",start=DEFAULT_START_DATE,end=DEFAULT_END_DATE):
     ds = db+"/"+stock
-    df_quandl = quandl.get(dataset=ds,start_date=start,end_date=end)
+    df_quandl = quandl.get(dataset=ds,
+                           start_date=start,
+                           end_date=end)
     return df_quandl
 
 def get_stock_intrinio(id="AAPL",start=DEFAULT_START_DATE,end=DEFAULT_END_DATE,freq="daily",page_s=10000):
-    r = security_api.get_security_stock_prices(identifier=id,start_date=start,frequency=freq,page_size=page_s)
+    r = security_api.get_security_stock_prices(identifier=id,
+                                               start_date=start,
+                                               frequency=freq,
+                                               page_size=page_s)
     #Structure the data as a dataframe
     response_list = [x.to_dict() for x in r.stock_prices]
     df_int = pd.DataFrame(response_list).sort_values("date")
@@ -57,15 +62,28 @@ def get_stock_intrinio(id="AAPL",start=DEFAULT_START_DATE,end=DEFAULT_END_DATE,f
     
 def get_stock_iexfinance(stock="AAPL",start=DEFAULT_START_DATE,end=DEFAULT_END_DATE,output_form="pandas"):
     from iexfinance.stocks import get_historical_data
-    df_iex = get_historical_data(stock,start,end,output_format=output_form,token=IEXFINANCE_KEY)
+    
+    df_iex = get_historical_data(stock,
+                                 start,
+                                 end,
+                                 output_format=output_form,
+                                 token=IEXFINANCE_KEY)
     return df_iex
     
 def get_stock_tiingo(stock="AAPL",start=DEFAULT_START_DATE,end=DEFAULT_END_DATE):
-    historical_p = tiingo_client.get_dataframe(stock, startDate=start, endDate=end,frequency="daily")
+    
+    historical_p = tiingo_client.get_dataframe(stock, 
+                                               startDate=start, 
+                                               endDate=end,
+                                               frequency="daily")
     return historical_p
 
 def get_stock_alpha_vantage(stock="AAPL",start=DEFAULT_START_DATE,end=DEFAULT_END_DATE):
     from alpha_vantage.timeseries import TimeSeries
+    
     ts = TimeSeries(key=ALPHAVANTAGE_KEY, output_format="pandas")
-    data, meta_data = ts.get_intraday(symbol=stock,interval='1min', outputsize='full')
+    
+    data, meta_data = ts.get_intraday(symbol=stock,
+                                      interval='1min', 
+                                      outputsize='full')
     return data
